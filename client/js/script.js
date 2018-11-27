51dd26bd7e18b726feb3deedd3df1a2448a68e62
Phuc Vu (Robert) <robertvu483@gmail.com>
2018-11-27 10:53:34 -0800

    Changed client layout

--- client/js/script.js
+++ client/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
+  createMovie('Family', './images/tombraider.jpg', 'Tomb Raider', 5, '<ul><li>Length: 120m</li><li>Director: Sissy</li></ul>', [ "abc", "sf sadf", "Robert", "Phuc Vu"]);
 });
 
-function createMovie(ribbon, img, name, rating, list) {
+function createMovie(ribbon, img, name, rating, list, actors) {
   var elm = '<div> <div> <div class="project"> <div class="ribbon">' + ribbon + '</div> <img src="' + img + '" alt="My Work"/> <div class="projectTitle"> <h5>' + name + '</h5> ';
   
   for (let i = 0; i < rating; i++) {
@@ -12,7 +13,13 @@ function createMovie(ribbon, img, name, rating, list) {
     elm += '<div> <div>☆</div> </div> ';
   }
                   
-  elm += '</div> ' + list + ' </div> </div>';
+  elm += '</div> ' + list + ' <p> Actors: ';
+  
+  for (let i = 0; i < actors.length; i++) {
+    elm += '<span>' + actors[i] + '</span> ';
+  }
+
+  elm += '</p> </div> </div>';
             
   $("#gallery > div").append(elm);
 }
