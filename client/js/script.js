21644f6be572f2b7fc95a764b12ad34f10f56368
Phuc Vu (Robert) <robertvu483@gmail.com>
2018-11-29 20:47:01 -0800

    Changed layout. Added animation

--- client/js/script.js
+++ client/js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-  createMovie('Family', './images/tombraider.jpg', 'Tomb Raider', 5, '<ul><li>Length: 120m</li><li>Director: Sissy</li></ul>', [ "abc", "sf sadf", "Robert", "Phuc Vu"]);
+  createMovie('Family', './images/tombraider.jpg', 'Tomb Raider', 5, '<ul><li>Length: 120m</li><li>Director: Sissy</li></ul>');
+  createTopMovies([ "abc", "abc", "abc", "abc", "abc" ]);
 });
 
 function createMovie(ribbon, img, name, rating, list, actors) {
-  var elm = '<div> <div> <div class="project"> <div class="ribbon">' + ribbon + '</div> <img src="' + img + '" alt="My Work"/> <div class="projectTitle"> <h5>' + name + '</h5> ';
+  let elm = '<div> <div> <div class="project"> <div class="ribbon">' + ribbon + '</div> <img src="' + img + '" alt="My Work"/> <div class="projectTitle"> <h5>' + name + '</h5> ';
   
   for (let i = 0; i < rating; i++) {
     elm += '<div> <div>★</div> </div> ';
@@ -13,13 +14,25 @@ function createMovie(ribbon, img, name, rating, list, actors) {
     elm += '<div> <div>☆</div> </div> ';
   }
                   
-  elm += '</div> ' + list + ' <p> Actors: ';
+  elm += '</div> ' + list + ' </div> </div>';/*' <p> Actors: ';
   
   for (let i = 0; i < actors.length; i++) {
     elm += '<span>' + actors[i] + '</span> ';
   }
 
-  elm += '</p> </div> </div>';
+  elm += '</p> </div> </div>';*/
             
   $("#gallery > div").append(elm);
+}
+
+function createTopMovies(list) {
+  let elm = "<table> <tbody> ";
+  
+  for (let i = 0; i < list.length; i++) {
+    elm += "<tr> <td> <span>" + (i + 1) + "</span> </td> <td>" + list[i] + "</td> </tr> "
+  }
+  
+  elm += "</tbody> </table>";
+  
+  $("#quote").before(elm);
 }
